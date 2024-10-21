51e376747a193dcc0afc99ea6846b04730c5de0e
FATBOY123456789 <ytzjimbuisness@gmail.com>
2024-10-21 18:24:52 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class AEAW:
+    __privateVAR=50
+    def __privateMETH(self):
+        print(AEAW.__privateVAR)
+    def hello(self):
+        AWWAWAW.__privateMETH()
+        print('Value is',AEAW.__privateVAR)
+AWWAWAW=AEAW()
+AWWAWAW.hello()
